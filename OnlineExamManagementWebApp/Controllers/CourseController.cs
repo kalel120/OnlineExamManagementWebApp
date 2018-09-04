7f74a28e79409a4ce6edc76d39305f7b92b11373
kalel120 <imran_aust_cse@hotmail.com>
2018-09-04 17:02:22 +0600

    Design basic UI with default layout

---
+++ OnlineExamManagementWebApp/Controllers/CourseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineExamManagementWebApp.Controllers {
+    public class CourseController : Controller {
+
+        public ActionResult Entry() {
+
+            return View();
+        }
+    }
+}
---
+++ OnlineExamManagementWebApp/ViewModels/CourseEntryViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using OnlineExamManagementWebApp.Models;
+
+namespace OnlineExamManagementWebApp.ViewModels {
+    public class CourseEntryViewModel {
+
+        public IEnumerable<SelectListItem> Organizations { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public int Duration { get; set; }
+
+        public double Credit { get; set; }
+
+        public string Outline { get; set; }
+
+        public IEnumerable<SelectListItem> Tags { get; set; }
+
+        public List<string> SelectedTags { get; set; }
+    }
+}
