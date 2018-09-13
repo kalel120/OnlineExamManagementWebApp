38fdc67322ad8fd085c065fdc1442dd81b2994ee
kalel120 <imran_aust_cse@hotmail.com>
2018-09-14 01:25:51 +0600

    Course >> Exam entry page basic UI

---
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/create-exams.js
@@ -0,0 +1,19 @@
+﻿
+$(function () {
+    var organizationId = $('#OrganizationId').val();
+    var courseId = $('#Id').val();
+
+    $("#js-examTypes").select2({
+        placeholder: 'Select a option',
+        theme: "classic",
+        selectOnClose: true
+    });
+
+    function loadCreateExamTab() {
+        $("js-organization").text($("#OrganizationCode").val());
+        $("js-course").val($("#Name").val());
+        $("js-exam-code").val($("#Code").val());
+    }
+
+    loadCreateExamTab();
+});
