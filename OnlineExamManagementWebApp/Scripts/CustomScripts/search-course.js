c4c2d838d1fc6eb5d19aed678394b362cbf132d3
imran <imran_aust_cse@hotmail.com>
2019-07-15 01:09:38 +0600

    Modal_Edit_Course >> basic markup done

--- OnlineExamManagementWebApp/Scripts/CustomScripts/search-course.js
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/search-course.js
@@ -6,33 +6,34 @@
 
         const jsTrainerList = $("#TrainerId");
 
-        $(document.body).on("change", "#OrganizationId",
-            function () {
-                const orgId = $(this).val();
+        $(document.body).on("change", "#OrganizationId", function () {
+            const orgId = $(this).val();
 
-                jsTrainerList.empty();
-                jsTrainerList.append(`<option value="0">--SELECT TRAINER--</option>`);
+            jsTrainerList.empty();
+            jsTrainerList.append(`<option value="0">--SELECT TRAINER--</option>`);
 
-                if (orgId != undefined && orgId !== "") {
-                    const json = { id: orgId };
+            if (orgId != undefined && orgId !== "") {
+                const json = { id: orgId };
 
-                    $.ajax({
-                        type: "POST",
-                        url: `/Course/GetTrainersByOrganization/${orgId}`,
-                        contentType: "application/json",
-                        dataType: "json",
-                        data: JSON.stringify(json),
+                $.ajax({
+                    type: "POST",
+                    url: `/Course/GetTrainersByOrganization/${orgId}`,
+                    contentType: "application/json",
+                    dataType: "json",
+                    data: JSON.stringify(json),
 
-                        success: (data) => {
-                            $.each(data, (key, value) => {
-                                jsTrainerList.append(`<option value="${value.Id}"> ${value.Name} </option>`);
-                            });
-                        },
+                    success: (data) => {
+                        $.each(data, (key, value) => {
+                            jsTrainerList.append(`<option value="${value.Id}"> ${value.Name} </option>`);
+                        });
+                    },
 
-                        error: () => alert("Something is wrong")
-                    });
-                }
-            });
+                    error: () => alert("Something is wrong")
+                });
+            }
+        });
 
+    
+    // jQuery Code Ends Here
     });
 })(jQuery);
