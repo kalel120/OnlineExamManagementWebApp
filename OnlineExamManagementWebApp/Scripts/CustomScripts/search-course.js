d15d6425d3a352f01ec078764055cb34d52c7baf
imran <imran_aust_cse@hotmail.com>
2019-07-23 13:12:57 +0600

    bind relevent data to edit course modal popup (except leadTrainer)

--- OnlineExamManagementWebApp/Scripts/CustomScripts/search-course.js
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/search-course.js
@@ -37,9 +37,29 @@
          *  Edit Course Modal PopUp
          */
         const editCourseModal = $("#js-modal-editCourse");
+        const getTableRowAsObject = (tableRow) => {
+            const row = {
+                Id: parseInt(tableRow.find("td:eq(0)").text()),
+                Name: tableRow.find("td:eq(1)").text(),
+                Code: tableRow.find("td:eq(2)").text(),
+                Duration: tableRow.find("td:eq(3)").text(),
+                Credit: tableRow.find("td:eq(5)").text(),
+                Outline: tableRow.find("td:eq(6)").text()             
+            }
+            return row;
+        }
+        const bindDataToEditCoursePopup = (data) => {
+            $("#modal-editCourse-Name").val(data.Name);
+            $("#modal-editCourse-Code").val(data.Code);
+            $("#modal-editCourse-Duration").val(data.Duration);
+            $("#modal-editCourse-Credit").val(data.Credit);
+            $("#modal-editCourse-Outline").val(data.Outline);
+        };
 
         $(document).on("click", ".js-editCourseModalPopup", (event) => {
             editCourseModal.modal("toggle");
+            console.log(getTableRowAsObject($(event.target).closest("tr")));
+            bindDataToEditCoursePopup(getTableRowAsObject($(event.target).closest("tr")));
         });
 
         /** End**/
