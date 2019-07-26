439c6518e631c960116ef983d9f867ad38f04249
imran <imran_aust_cse@hotmail.com>
2019-07-26 15:07:04 +0600

    fetch trainer info from server for binding with modal_dropdown

--- OnlineExamManagementWebApp/Scripts/CustomScripts/search-course.js
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/search-course.js
@@ -37,15 +37,39 @@
          *  Edit Course Modal PopUp
          */
         const editCourseModal = $("#js-modal-editCourse");
+
+        const fetchTrainers = (courseId) => {
+            let trainers = [];
+
+            const promiseCourseTrainer = new Promise((resolve, reject) => {
+                $.get(`/Course/GetTrainersByCourse/${courseId}`)
+                    .done((data) => {
+                        resolve(data);
+                    });
+            });
+
+            promiseCourseTrainer.then((result) => {
+                $.each(result, (key, value) => {
+                    let jsonData = {};
+                    let columnName = value.TrainerId;
+                    jsonData[columnName] = value.TrainerName;
+                    trainers.push(jsonData);
+                });
+            });
+            return trainers;
+        };
+
         const getTableRowAsObject = (tableRow) => {
-            const row = {
-                Id: parseInt(tableRow.find("td:eq(0)").text()),
+            const courseId = parseInt(tableRow.find("td:eq(0)").text());
+            row = {
+                Id: courseId,
                 Name: tableRow.find("td:eq(1)").text(),
                 Code: tableRow.find("td:eq(2)").text(),
                 Duration: tableRow.find("td:eq(3)").text(),
                 Credit: tableRow.find("td:eq(5)").text(),
-                Outline: tableRow.find("td:eq(6)").text()             
+                Outline: tableRow.find("td:eq(6)").text()
             }
+            row["Trainers"] = fetchTrainers(courseId);
             return row;
         }
         const bindDataToEditCoursePopup = (data) => {
@@ -58,7 +82,6 @@
 
         $(document).on("click", ".js-editCourseModalPopup", (event) => {
             editCourseModal.modal("toggle");
-            console.log(getTableRowAsObject($(event.target).closest("tr")));
             bindDataToEditCoursePopup(getTableRowAsObject($(event.target).closest("tr")));
         });
 
