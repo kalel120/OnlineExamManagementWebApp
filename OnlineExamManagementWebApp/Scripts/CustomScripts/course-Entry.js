0161215ce52775cec0228493250c66ad42171410
imran <imran_aust_cse@hotmail.com>
2019-08-28 23:34:43 +0600

    Added course entry js file

---
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/course-Entry.js
@@ -0,0 +1,18 @@
+﻿// I used IIFE around the jQuery code to avoid conflict with other js libraries
+
+(() => {
+    $(() => {
+        $("#org-list").select2({
+            placeholder: "--Select Organization--"
+        });
+
+        $("#tag-list").select2({
+            tags: true,
+            tokenSeparators: [',', ' '],
+            placeholder: "Select or Create Tags"
+
+        });
+
+        $(".close").alert();
+    });
+})(jQuery);
