396376a9a94e3055a945bf511446653488a32cab
imran <imran_aust_cse@hotmail.com>
2019-09-03 16:51:27 +0600

    Added js file and viewmodel for org entry page

---
+++ OnlineExamManagementWebApp/ViewModels/OrgEntryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace OnlineExamManagementWebApp.ViewModels {
+    public class OrgEntryViewModel {
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public string Address { get; set; }
+
+        public string Contact { get; set; }
+
+        public string About { get; set; }
+
+        public HttpPostedFileBase Logo { get; set; }
+    }
+}
