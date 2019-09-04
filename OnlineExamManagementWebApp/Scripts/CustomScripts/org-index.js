82c5ab46f1c7c53cc30817d2ae3a1bbdeb20ff3e
imran <imran_aust_cse@hotmail.com>
2019-09-04 21:28:49 +0600

    Delete organization from list >> frontend done

--- OnlineExamManagementWebApp/Scripts/CustomScripts/org-index.js
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/org-index.js
@@ -3,5 +3,58 @@
 (($) => {
     $(() => {
         $("#dataTable-orgList").DataTable();
+
+        const deleteOrgDialog = $("#js-deleteOrg-dialog");
+        deleteOrgDialog.hide();
+
+        const deleteOrgById = (orgId) => {
+            $.post("/Organization/Delete/", { orgId: orgId })
+                .done((data) => {
+                    alert("Deleted Successfully");
+                    location.reload(true);
+                })
+                .fail((jqXHR, textStatus) => {
+                    alert(`Error >>${textStatus}`);
+                });
+        }
+
+        $(document).on("click", ".js-deleteOrgPopup", (event) => {
+            const orgId = $("#OrgId").val();
+
+            deleteOrgDialog.dialog({
+                resizable: false,
+                height: "auto",
+                width: 400,
+                modal: true,
+                show: {
+                    effect: "puff",
+                    duration: 1000
+                },
+                hide: {
+                    effect: "explode",
+                    duration: 1000
+                },
+                buttons: [
+                    {
+                        text: "Delete?",
+                        open: function () {
+                            $(this).addClass("cancelClass");
+                        },
+                        click: function () {
+                            deleteOrgById(orgId);
+                            $(this).dialog("close");
+                        }
+                    }, {
+                        text: "Cancel",
+                        open: function () {
+                            $(this).addClass("confirmClass");
+                        },
+                        click: function () {
+                            $(this).dialog("close");
+                        }
+                    }
+                ]
+            });
+        });
     });
 })(jQuery);
