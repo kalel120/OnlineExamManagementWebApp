1d79dfc365564032dffbe143d4e24aa93f653d64
imran <imran_aust_cse@hotmail.com>
2019-09-11 01:10:01 +0600

    created listOfTrainersJs file. Soft delete trainers front end functionality. Backend not done yet

---
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/listOfTrainers.js
@@ -0,0 +1,65 @@
+﻿// IIFE
+(($) => {
+    $(() => {
+        $("#tbl-listOfTrainers").DataTable();
+        const deleteTrainerDialog = $("#js-deleteTrainer-dialog");
+        deleteTrainerDialog.hide();
+
+        // Delete trainer with jquery UI dialog
+        const deleteTrainerById = (id) => {
+            $.post("/.../Delete/", { orgId: id })
+                .done((data) => {
+                    if (data) {
+                        alert("Deleted Successfully");
+                        location.reload(true);
+                    } else {
+                        alert("Deletion failed");
+                    }
+                })
+                .fail((jqXhr, textStatus) => {
+                    alert(`Error >>${textStatus}`);
+                });
+        }
+
+        $(document).on("click", ".js-deleteTrainerPopup", (event) => {
+            const trainerId = $(event.target).closest("tr").find('input[type="hidden"]').val();
+
+            deleteTrainerDialog.dialog({
+                resizable: false,
+                height: "auto",
+                width: 400,
+                modal: true,
+                show: {
+                    effect: "puff",
+                    duration: 250
+                },
+                hide: {
+                    effect: "explode",
+                    duration: 250
+                },
+                buttons: [
+                    {
+                        text: "Delete?",
+                        open: function () {
+                            $(this).addClass("cancelClass");
+                        },
+                        click: function () {
+                            deleteTrainerById(trainerId);
+                            $(this).dialog("close");
+                        }
+                    }, {
+                        text: "Cancel",
+                        open: function () {
+                            $(this).addClass("confirmClass");
+                        },
+                        click: function () {
+                            $(this).dialog("close");
+                        }
+                    }
+                ]
+            });
+        });
+        // delete trainer functionality End
+    });
+})(jQuery);
+
