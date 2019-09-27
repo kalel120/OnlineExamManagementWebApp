8c642ce6dc34dd5dead367fd99f1b7ca65a9382e
imran <imran_aust_cse@hotmail.com>
2019-09-27 23:27:05 +0600

    Removed: unnecessary props from SearchOrgViewModel

--- OnlineExamManagementWebApp/ViewModels/SearchOrgViewModel.cs
+++ OnlineExamManagementWebApp/ViewModels/SearchOrgViewModel.cs
@@ -1,31 +1,16 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using OnlineExamManagementWebApp.Models;
 
 namespace OnlineExamManagementWebApp.ViewModels {
     public class SearchOrgViewModel {
-        public int Id { get; set; }
-
-        [Required]
-        [MaxLength(20)]
         public string Name { get; set; }
 
         public string Code { get; set; }
 
-        [Required]
-        [MaxLength(20)]
         public string Address { get; set; }
 
-        [Required]
-        [MaxLength(20)]
         public string Contact { get; set; }
 
-        public string About { get; set; }
-
         public List<Organization> Organizations { get; set; }
-
-        public List<Course> Courses { get; set; }
-
-        public List<Trainer> Trainers { get; set; }
     }
 }
