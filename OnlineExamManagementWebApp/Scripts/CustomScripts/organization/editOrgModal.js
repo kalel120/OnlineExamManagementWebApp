ae80d037ff18abdeafd257a52508bb2daeb88d06
imran <imran_aust_cse@hotmail.com>
2019-09-28 16:11:02 +0600

    bind table data to edit org modal

--- OnlineExamManagementWebApp/Scripts/CustomScripts/organization/editOrgModal.js
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/organization/editOrgModal.js
@@ -4,8 +4,27 @@
     $(() => {
         const editOrgModal = $("#js-modal-editOrg");
 
+        const getTableRowAsObject = (tableRow) => {
+            return {
+                Id: tableRow.find("td:eq(7)").children(".js-viewOrgModalPopup").attr("data-org-id"),
+                Name: tableRow.find("td:eq(1)").text(),
+                Code: tableRow.find("td:eq(2)").text(),
+                Address: tableRow.find("td:eq(3)").text(),
+                Contact: tableRow.find("td:eq(4)").text()
+            };
+        }
+
+        const bindToEditOrgModal = (data) => {
+            $("#js-modal-editOrg-name").val(data.Name);
+            $("#js-modal-editOrg-code").val(data.Code);
+            $("#js-modal-editOrg-address").val(data.Address);
+            $("#js-modal-editOrg-contact").val(data.Contact);
+        }
+
         $(document).on("click", ".js-editOrgModalPopup", (event) => {
             setTimeout(() => {
+                let tableRow = getTableRowAsObject($(event.target).closest("tr"));
+                bindToEditOrgModal(tableRow);
                 editOrgModal.modal("toggle");
             },300);
         });
