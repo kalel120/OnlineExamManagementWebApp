ea5dc019752aad25436833dd8a7a8b003c5c62b8
imran <imran_aust_cse@hotmail.com>
2019-09-29 01:17:06 +0600

    org update >> markup and js done

--- OnlineExamManagementWebApp/Scripts/CustomScripts/organization/editOrgModal.js
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/organization/editOrgModal.js
@@ -3,6 +3,7 @@
 (($) => {
     $(() => {
         const editOrgModal = $("#js-modal-editOrg");
+        const btnEditOrgSubmit = $("#js-modal-editOrg-Submit");
 
         const getTableRowAsObject = (tableRow) => {
             return {
@@ -19,6 +20,7 @@
             $("#js-modal-editOrg-code").val(data.Code);
             $("#js-modal-editOrg-address").val(data.Address);
             $("#js-modal-editOrg-contact").val(data.Contact);
+            btnEditOrgSubmit.data("orgId", data.Id);
         }
 
         $(document).on("click", ".js-editOrgModalPopup", (event) => {
@@ -26,7 +28,18 @@
                 let tableRow = getTableRowAsObject($(event.target).closest("tr"));
                 bindToEditOrgModal(tableRow);
                 editOrgModal.modal("toggle");
-            },300);
+            }, 300);
+        });
+
+
+        const updateOrganization = async (content, orgId) => {
+            let result = await $.post("/url", {});
+            console.log(result);
+        }
+
+        btnEditOrgSubmit.on("click", () => {
+            const updatedModalContents = $("#js-modal-editOrg-form").serializeArray();
+            updateOrganization(updatedModalContents, btnEditOrgSubmit.data("orgId"));
         });
     });
 })(jQuery);
