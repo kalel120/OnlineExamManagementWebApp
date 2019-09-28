9cacb3288333e9fca24613310399d3585f9e2f13
imran <imran_aust_cse@hotmail.com>
2019-09-28 15:43:09 +0600

    Added >> edit org modal markup and toggle functionality

---
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/organization/editOrgModal.js
@@ -0,0 +1,13 @@
+﻿// IIFE
+
+(($) => {
+    $(() => {
+        const editOrgModal = $("#js-modal-editOrg");
+
+        $(document).on("click", ".js-editOrgModalPopup", (event) => {
+            setTimeout(() => {
+                editOrgModal.modal("toggle");
+            },300);
+        });
+    });
+})(jQuery);
