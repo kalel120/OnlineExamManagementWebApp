ed5552c3245d175fef0c752dd8dbe353ef2eca23
imran <imran_aust_cse@hotmail.com>
2019-10-15 00:08:31 +0600

    Added custom properties in AppUser domain model

--- OnlineExamManagementWebApp.Models/Identity/AppUser.cs
+++ OnlineExamManagementWebApp.Models/Identity/AppUser.cs
@@ -3,8 +3,14 @@
 namespace OnlineExamManagementWebApp.Models.Identity {
     public class AppUser : IdentityUser<int, IdentityUserLogin<int>, AppUserRole, IdentityUserClaim<int>> {
 
-        public AppUser() {
-
-        }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string AlternateAddress { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string Profession { get; set; }
+        public string HighestAcademic { get; set; }
+        public byte[] Image { get; set; }
     }
 }
