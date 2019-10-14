7cd6063e2cd53e52f70c7bf087bacae2cc1457c1
imran <imran_aust_cse@hotmail.com>
2019-10-15 00:33:45 +0600

    Added AppUserManager, AppRoleManager >> Extends AspNet Identity Framework

--- OnlineExamManagementWebApp.BLL/Identity/AppRoleManager.cs
+++ OnlineExamManagementWebApp.BLL/Identity/AppRoleManager.cs
@@ -1,5 +1,11 @@
-﻿namespace OnlineExamManagementWebApp.BLL.Identity {
-    public class AppRoleManager {
+﻿using Microsoft.AspNet.Identity;
+using OnlineExamManagementWebApp.Models.Identity;
 
+namespace OnlineExamManagementWebApp.BLL.Identity {
+    public class AppRoleManager : RoleManager<AppRole, int> {
+
+        public AppRoleManager(IRoleStore<AppRole, int> store) : base(store) {
+
+        }
     }
 }
--- OnlineExamManagementWebApp.BLL/Identity/AppUserManager.cs
+++ OnlineExamManagementWebApp.BLL/Identity/AppUserManager.cs
@@ -1,5 +1,11 @@
-﻿namespace OnlineExamManagementWebApp.BLL.Identity {
-    public class AppUserManager {
+﻿using Microsoft.AspNet.Identity;
+using OnlineExamManagementWebApp.Models.Identity;
 
+namespace OnlineExamManagementWebApp.BLL.Identity {
+    public class AppUserManager : UserManager<AppUser, int> {
+
+        public AppUserManager(IUserStore<AppUser, int> store) : base(store) {
+
+        }
     }
 }
