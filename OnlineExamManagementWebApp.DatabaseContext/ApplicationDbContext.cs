cf3dcfdb9c7422decfc066bfae146ace03978aaf
imran <imran_aust_cse@hotmail.com>
2019-10-15 22:55:34 +0600

    installed AspNet.Identity.Ef on repo layer. Reffered IdentityDbContext as dbContext

--- OnlineExamManagementWebApp.DatabaseContext/ApplicationDbContext.cs
+++ OnlineExamManagementWebApp.DatabaseContext/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
 ﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using OnlineExamManagementWebApp.Models;
+using OnlineExamManagementWebApp.Models.Identity;
 
 namespace OnlineExamManagementWebApp.DatabaseContext {
-    public class ApplicationDbContext : DbContext {
+    public class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, int, IdentityUserLogin<int>, AppUserRole, IdentityUserClaim<int>> {
+
+        public ApplicationDbContext() : base("ApplicationDbContext") {
+
+        }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Exam> Exams { get; set; }
