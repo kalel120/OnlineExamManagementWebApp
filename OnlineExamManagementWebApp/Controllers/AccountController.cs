22b29de7ea61118b366d03d5d94f3d9d3af47d30
imran <imran_aust_cse@hotmail.com>
2019-10-25 00:58:27 +0600

    Removed webApi controller for Account Creation. Added mvc controller

---
+++ OnlineExamManagementWebApp/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using OnlineExamManagementWebApp.BLL.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace OnlineExamManagementWebApp.Controllers {
+    public class AccountController : Controller {
+
+        public AppUserManager UserManager {
+            get {
+                return HttpContext.GetOwinContext().Get<AppUserManager>();
+            }
+        }
+
+        public AppRoleManager RoleManager {
+            get {
+                return HttpContext.GetOwinContext().Get<AppRoleManager>();
+
+            }
+        }
+
+        public AppSignInManager SignInManager {
+            get {
+                return HttpContext.GetOwinContext().Get<AppSignInManager>();
+
+            }
+        }
+
+        /* Registration */
+        public ActionResult Register() {
+            return View();
+        }
+    }
+}
