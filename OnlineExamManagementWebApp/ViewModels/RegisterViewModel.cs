258a1236e48540506f3c9bdb8257423fbb848ef9
imran <imran_aust_cse@hotmail.com>
2019-12-26 23:24:42 +0600

    Added client side validation on register account page

--- OnlineExamManagementWebApp/ViewModels/RegisterViewModel.cs
+++ OnlineExamManagementWebApp/ViewModels/RegisterViewModel.cs
@@ -2,6 +2,8 @@
 
 namespace OnlineExamManagementWebApp.ViewModels {
     public class RegisterViewModel {
+        [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string Username { get; set; }
 
         [Required]
