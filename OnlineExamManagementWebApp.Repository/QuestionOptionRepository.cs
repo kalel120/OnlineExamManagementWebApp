fc7d6f553076aacb5811deb0b4bef4378225db95
imran <imran_aust_cse@hotmail.com>
2020-03-16 01:49:21 +0600

    Added question, options and join tables repository

---
+++ OnlineExamManagementWebApp.Repository/QuestionOptionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineExamManagementWebApp.DatabaseContext;
+
+namespace OnlineExamManagementWebApp.Repository {
+    public class QuestionOptionRepository {
+        private readonly ApplicationDbContext _dbContext;
+        public QuestionOptionRepository(ApplicationDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+    }
+}
---
+++ OnlineExamManagementWebApp.Repository/QuestionRepository.cs
@@ -0,0 +1,11 @@
+﻿using OnlineExamManagementWebApp.DatabaseContext;
+
+namespace OnlineExamManagementWebApp.Repository {
+    public class QuestionRepository {
+        private readonly ApplicationDbContext _dbContext;
+
+        public QuestionRepository(ApplicationDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+    }
+}
