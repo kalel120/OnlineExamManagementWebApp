7636deeafa64bcc443378ce1618672d611f95d31
imran <imran_aust_cse@hotmail.com>
2020-03-16 01:13:46 +0600

    Added Q&A controller and Q&A viewmodel

---
+++ OnlineExamManagementWebApp/Controllers/QuestionAnswerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineExamManagementWebApp.Controllers {
+    public class QuestionAnswerController : Controller {
+
+        // Get
+        public ActionResult QuestionAnswer(int examId) {
+            return View();
+        }
+    }
+}
---
+++ OnlineExamManagementWebApp/ViewModels/QuestionAnswerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineExamManagementWebApp.ViewModels {
+    public class QuestionAnswerViewModel {
+        public string OrganizationName { get; set; }
+        public string CourseName { get; set; }
+        public string ExamCode { get; set; }
+        public string QuestionDescription { get; set; }
+    }
+}
