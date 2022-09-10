af467101150087706c9fa975e17ffe948e4b43dd
imran <imran_aust_cse@hotmail.com>
2022-09-10 13:53:27 +0600

    QuestionAnswer Entry section >> frontend >> Bind questions table data to view questions modal popup

--- OnlineExamManagementWebApp/Scripts/CustomScripts/qa/questionAnswerViewModal.js
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/qa/questionAnswerViewModal.js
@@ -6,13 +6,40 @@
          */
         const qoViewModal = $("#js-modal-viewQo");
 
+        $("#js-modal-viewQo-qText").on("change", function() {
+            $(this).height(0).height(this.scrollHeight);
+        }).find("js-modal-viewQo-qText").trigger("change");
         // End
 
-        // Popup Question Option Modal
 
-        $(document).on("click",".js-qoModalPopup", function(event) {
+        const getRowOfQuestionTableAsObject = function(row) {
+            let content = {
+                QuestionId: row.find("a.btn.btn-primary.js-qoModalPopup").attr("data-question-id"),
+                Serial: $.trim(row.find("td:eq(0)").text()),
+                Marks: $.trim(row.find("td:eq(1)").text()),
+                Description: $.trim(row.find("td:eq(2)").text()),
+                OptionType: $.trim(row.find("td:eq(3)").text())
+            };
+
+            return content;
+        };
+
+        const bindToViewQoModal = function(data) {
+            $("#js-modal-viewQo-order").val(data.Serial);
+            $("#js-modal-viewQo-marks").val(data.Marks);
+            $("#js-modal-viewQo-qText").val(data.Description);
+            $("#js-modal-viewQo-optionType").val(data.OptionType);
+        };
+
+
+
+        // Popup Question Option Modal
+        $(document).on("click", ".js-qoModalPopup", function (event) {
+            
             setTimeout(function(){
-                console.log("Question ID >>"+$(event.target).data("data-question-id"));
+                let tableRow = getRowOfQuestionTableAsObject($(event.target).closest("tr"));
+                bindToViewQoModal(tableRow);
+                
                 qoViewModal.modal("toggle");
             }, 300);
         });
