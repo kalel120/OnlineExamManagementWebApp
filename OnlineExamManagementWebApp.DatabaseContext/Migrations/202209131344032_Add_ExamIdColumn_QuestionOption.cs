1de1f807caec3fd5227c09708dc8506d50d26214
imran <imran_aust_cse@hotmail.com>
2022-09-13 19:52:01 +0600

    Database update complete for adding ExamId column

---
+++ OnlineExamManagementWebApp.DatabaseContext/Migrations/202209131344032_Add_ExamIdColumn_QuestionOption.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineExamManagementWebApp.DatabaseContext.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Add_ExamIdColumn_QuestionOption : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.QuestionOptions", "ExamId", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.QuestionOptions", "ExamId");
+        }
+    }
+}
