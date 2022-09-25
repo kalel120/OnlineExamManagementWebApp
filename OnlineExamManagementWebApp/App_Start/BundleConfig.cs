dc1d55ff5d7b2aadf68d3c6f74e0195fb8efa98e
imran <imran_aust_cse@hotmail.com>
2022-09-25 17:04:56 +0600

    Theme cleanup >> Referenced bundle for jquery-ui

--- OnlineExamManagementWebApp/App_Start/BundleConfig.cs
+++ OnlineExamManagementWebApp/App_Start/BundleConfig.cs
@@ -6,14 +6,16 @@ namespace OnlineExamManagementWebApp {
 
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
