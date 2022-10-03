387e0728ed948efa1aba24c8ec0fad082fe9e354
imran <imran_aust_cse@hotmail.com>
2022-10-03 19:53:29 +0600

    QuestionAnswerEntry section >> Remove Question >> Use jqDT method to remove question from table on client

--- OnlineExamManagementWebApp/Scripts/CustomScripts/qa/questionAnswerRemoveQuestion.js
+++ OnlineExamManagementWebApp/Scripts/CustomScripts/qa/questionAnswerRemoveQuestion.js
@@ -2,12 +2,18 @@
 
 (($) => {
     $(() => {
+        // Initialization
+        const $QuestionsDt = $('#tbl-questions').DataTable();
+
         // For every call of this function, questions serial no. will reset using loop index
         const reOrderQuestionSerialOnClient = function () {
             $("#tbl-questions").find("tbody tr td:nth-child(1)").each(function (index, element) {
-                console.log(typeof index);
                 $(element).text(index + 1);
             });
+        };
+
+        const removeQuestionOnServer = function(questionId) {
+
         };
 
         $(document).on("click", ".js-removeQuestion", function (event) {
@@ -24,18 +30,19 @@
                     }
 
                     // remove from questions table (client)
-                    closestRow.remove();
+                    $QuestionsDt.rows(closestRow).remove().draw();
 
                     // re-order serial no. on client
                     reOrderQuestionSerialOnClient();
 
                     // marked as removed on server
+                    console.log(closestRow.find("td:eq(5) > a").data("question-id"));
                     // re-sequence on server
                     // refresh questions table (from serve)
-                    if (typeof localStorage !== "undefined") {
-                        localStorage.setItem("success", "Question Removed");
-                    }
-                    window.location.reload();
+                    //if (typeof localStorage !== "undefined") {
+                    //    localStorage.setItem("success", "Question Removed");
+                    //}
+                    //window.location.reload();
                 }
             });
         });
